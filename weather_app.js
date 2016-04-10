72527c18b52bae1331db8ae998478408c2648fb6
Jaren831 <jaren.lynchmobb@gmail.com>
2016-04-09 22:12:47 -0700

    Adds openweather API and geolocation function - Currently does not change html - Not working

--- weather_app.js
+++ weather_app.js
@@ -1,12 +1,51 @@
-var openweatherKet = "af0ba94d81149880f9d5d4a1ed5ba600";
+$(document).ready(function() {
+	var Geo = {};
+	var key = "af0ba94d81149880f9d5d4a1ed5ba600";
+	function getLocation() {
+	  // Request location consent from user
+	  if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(success, error);
+	  } else {
+	    alert("Geolocation is not supported!");
+	  }
+	};
+
+	function error() {
+		alert("Location not found...");
+	};
+
+	function success(position) {
+		//Assigns latitude and latitude to Geo object.
+		Geo.lat = position.coords.latitude;
+		Geo.lng = position.coords.longitude;
+	}
+	var weather = "api.openweathermap.org/data/2.5/weather?lat=" + Geo.lat + "&" + Geo.lng + "&appid=" + key;
+	//Pulls data from openweather api.
+	$.ajax({
+		url: weather,
+		dataType: "jsonp",
+		success: function(data) {
+			//Get data.
+			var temp = data["main"]["temp"];
+			var city = data["name"];
+			var country = data["sys"]["country"];
+		}
+	})
+	//Api gives kelvin. Converts to celsius and fahrenheit.
+	function convert(temp) {
+		if (document.getElementById("fahrenheit").checked) {
+			function convertF(temp) {
+				var fTemp = temp * (9/5) - 459.67;
+				document.getElementById("temperatureBox").innerHTML = fTemp;
+		} else if (document.getElementById("celsius").checked) {
+			function convertC(temp) {
+				var cTemp = temp - 273.15;
+				document.getElementById("temperatureBox").innerHTML = cTemp;
+				}
+			}
+		}
+	}
+	document.getElementById("location").innerHTML = city + " , " + country;
+});
 
 
-function getLocation() {
-  // Request location consent from user
-  var display = document.getElementById('weather');
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    display.innerHTML = 'Geolocation is not supported by this browser.';
-  }
-};
