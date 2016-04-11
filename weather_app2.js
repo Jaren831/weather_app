5df166b616d111ba854dfe6728fe0f0621d2a529
Jaren831 <jaren.lynchmobb@gmail.com>
2016-04-10 22:11:32 -0700

    Functional - Displays icon depending on weather - Unit radio buttons work. Changes units based on selection - Is fucking ugly

--- weather_app2.js
+++ weather_app2.js
@@ -1,20 +1,32 @@
-var getLocation = function(data) {
+function getLocation(data) {
+	var weather = "http://api.openweathermap.org/data/2.5/weather?lat=";
 	var key = "af0ba94d81149880f9d5d4a1ed5ba600";
 	var lat = data.lat;
 	var lon = data.lon;
 	var city = data.city;
 	var country = data.country;
-	var weather = "api.openweathermap.org/data/2.5/weather?lat=" + lat + "&" + lng + "&appid=" + key;
-	getWeather = function(data) {
-		var temp = data.main.temp;
-		var icon = data.weather.icon;
-		document.getElementById("weatherIcon").src = "http://openweathermap.org/img/w/" + icon + ".png"
-		document.getElementById("temperatureBox").innerHTML = temp;
-	};
+	weather = weather + lat + "&" + "lon=" + lon + "&appid=" + key;
 	document.getElementById("location").innerHTML = city + " , " + country;
+	function getWeather(data) {
+		var temp = data.main.temp;
+		var icon = data.weather[0].icon;
+		if (document.getElementById("fahrenheit").checked) {
+			temp = Math.floor(temp * (9/5) - 459.67);
+			document.getElementById("temperatureBox").innerHTML = temp + "\u00B0" + " F";
+		} else {
+			temp = Math.floor(temp - 273.15);
+			document.getElementById("temperatureBox").innerHTML = temp + "\u00B0" + " C";
+		}
+		document.getElementById("weatherIcon").src = "http://openweathermap.org/img/w/" + icon + ".png"
+	};
 	$.getJSON(weather, getWeather);
+
 };
 
 $(document).ready(function() {
+	$.getJSON("http://ip-api.com/json", getLocation);
+});
+
+$("input").click(function() {
 	$.getJSON("http://ip-api.com/json", getLocation);
 });
